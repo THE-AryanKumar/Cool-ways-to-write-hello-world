c4107f1f8e3e66af4108491ae5a3373e59d11636
Aryan <lit2021030@iiitl.ac.in>
2022-10-01 21:52:15 +0530

    helloworld #hacktoberfest2022

---
+++ helloworld3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+    
+int main(){
+    cout<<"h";
+    cout<<"e";
+    cout<<"l";
+    cout<<"l";
+    cout<<"o";
+    cout<<" ";
+    cout<<"w";
+    cout<<"o";
+    cout<<"r";
+    cout<<"l";
+    cout<<"d";
+
+    return 0;
+}
